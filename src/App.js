9b94bcc9c9691e65e66f30a04129388017b5b7ce
Azeez Ariyibi <Olabisi14>
2023-09-12 14:10:05 +0100

    Hello

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/home' element={<Home />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
       <Footer />
